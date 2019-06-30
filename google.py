0f0459c60cb9b260e3ee4c9b37108c4501c9c3c0
Ulysse Darmet <darmet.ulysse@gmail.com>
2019-06-30 16:43:31 +0200

    Fix outdated google parsing for episode names

--- google.py
+++ google.py
@@ -45,21 +45,11 @@ def fetch_series_season_titles(series_name, season_no, search_language):
 	query_result = requests.get(query_url)
 	soup = BeautifulSoup(query_result.text, 'html.parser')
 	
-	for g in soup.find(id='search').find_all(class_='g'):
-
-		abort_on_next_it = False
-
-		for span in g.find_all('span'):
-			text = span.get_text()
-			match = re.search('S(?P<season_no>\d+\w*)\s*E(?P<eps_no>\d+\w*)\W*(?P<eps_title>.*)\Z', text, re.IGNORECASE)
-			if match is None:
-				continue
-			eps_no = match.group('eps_no').lstrip('0')
-			eps_title = match.group('eps_title')
-			abort_on_next_it = True
-			yield eps_no, eps_title
-		
-		if abort_on_next_it:
-			return
+	eps_re = re.compile('^S(?P<season_no>\d+\w*)\s*E(?P<eps_no>\d+\w*)\W*(?P<eps_title>.*)\Z', re.IGNORECASE)
+	for text in soup.find_all(text=eps_re):
+		match = eps_re.match(text)
+		eps_no = match.group('eps_no').lstrip('0')
+		eps_title = match.group('eps_title')
+		yield eps_no, eps_title
 
 
